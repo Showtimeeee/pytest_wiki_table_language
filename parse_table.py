24e2b8260c6a18c903066814dcfff8d7ed89db82
Showtimeeeeexitgit config --global user.email sipovichit@yandex.rugit config --global user.name Showtimeeee <sipovichit@yandex.ru>
2023-10-27 22:45:54 +0300

    parse columns

---
+++ parse_table.py
@@ -0,0 +1,68 @@
+import requests
+from bs4 import BeautifulSoup
+from typing import List
+from dataclasses import dataclass
+import re
+
+
+@dataclass
+class ProgrammingLanguage:
+    name: str
+    frontend: str
+    backend: str
+    popularity: int
+
+
+
+url = "https://en.wikipedia.org/wiki/Programming_languages_used_in_most_popular_websites"
+response = requests.get(url)
+html = response.text
+
+soup = BeautifulSoup(html, "html.parser")
+
+table = soup.find("table", {"class": "wikitable"})
+
+
+data = []
+for row in table.find_all("tr")[1:]:
+    columns = row.find_all(["th", "td"])
+
+
+    name = columns[0].text.strip()
+
+    print(name)
+
+
+    if columns[1].find("a"):
+        frontend = columns[1].find("a").text.strip()
+    else:
+        frontend = columns[1].text.strip()
+
+    if columns[2].find("a"):
+        backend = columns[2].find("a").text.strip()
+    else:
+        backend = columns[2].text.strip()
+
+    popularity_text = columns[3].text.strip().replace(",", "")
+    try:
+        popularity = int(popularity_text.split()[0])
+    except (IndexError, ValueError):
+        popularity = 0
+
+    popularity_text = columns[3].text.strip().replace(",", "")
+
+    # взять все цифры из строки
+    popularity = re.sub(r'\D', '', popularity_text)
+
+    if popularity:
+        popularity = int(popularity)
+    else:
+        popularity = 0
+
+    data.append(ProgrammingLanguage(name, frontend, backend, popularity))
+
+
+
+print(frontend)
+print(backend)
+print(popularity)
